388b63dd154a4c32ce0a6a05772d225b0522c2c1
tylerjohnhaden <tylerjohnhaden@gmail.com>
2017-11-28 21:36:38 -0500

    Complete refactor of project design

---
+++ metassistant/__init__.py
@@ -0,0 +1,23 @@
+import socket
+import os
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+def settings(**kwargs):
+    if 'INSTALLED_APPS' in kwargs:
+        # todo: add config state check
+        return get_apps()
+
+    if 'ALLOWED_HOSTS' in kwargs:
+        # todo: add config state check
+        return ['localhost', '127.0.0.1'] \
+               + [i[4][0] for i in socket.getaddrinfo(socket.gethostname(), None) if i[0] == socket.AF_INET]
+
+
+def urls():
+    # todo: add config state check
+    return ['{0}.urls'.format(a) for a in get_apps()]
+
+
+def get_apps():
+    return os.listdir(os.path.join(__file__, os.pardir, os.pardir, 'apps'))
